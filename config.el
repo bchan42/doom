88c4a11573ddbbc30cf3fb140c47104a1aa1cfb2
bchan42 <bchan3k@gmail.com>
2025-03-27 22:13:26 -0700

    add org-roam config

--- config.el
+++ config.el
@@ -35,7 +35,9 @@
 ;; (setq doom-theme 'doom-one)
 
 (setq doom-theme  'doom-solarized-dark) ;; change to another theme
-;; other popular themes: doom-one, doom-gruvbox, doom-dracula, doom-nord, doom-solarized-dark, doom-soloraized-light, doom-material, doom-challenger-deep, doom-vibrant, doom-losvkem
+;; other popular themes: doom-one, doom-gruvbox, doom-dracula, doom-nord,
+;; doom-solarized-dark, doom-soloraized-light, doom-material, doom-challenger-deep,
+;; doom-vibrant, doom-losvkem
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
@@ -43,7 +45,91 @@
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/org/")
+(setq org-directory "~/notes/org/") ;; different location
+
+
+(setq org-roam-directory (file-truename "~/notes/roam/")) ;; add config to org-roam
+(setq org-roam-file-extensions '("org")) ;; add file extensions
+(setq org-roam-node-display-template
+      (concat "${type:15} ${title:*} " (propertize "${tags:10}" 'face 'org-tag))) ;; add templates for different types of notes
+
+(setq org-roam-capture-templates
+      '(("m" "main" plain "%?"
+         :if-new (file+head "main/${slug}.org"
+                            "#+title: ${title}\n") ;; main notes
+         :immediate-finish t
+         :unnarrowed t)
+        ("r" "reference" plain "%?"
+         :if-new (file+head "reference/${title}.org"
+                            "#+title: ${title}\n") ;; reference notes
+         :immediate-finish t
+         :unnarrowed t)
+        ("d" "default" plain "%?"
+         :if-new (file+head "%<%Y%m%d%H%M%S>-${slug}.org"
+                            "#+title: ${title}\n") ;; default notes
+        ("math" "math413" plain "%?"
+         :if-new (file+head "math413/${slug}.org"
+                            "#+title: ${title}\n") ;; math413 notes
+         :immediate-finish t
+         :unnarrowed t)
+        ("stat" "stat419" plain "%?"
+         :if-new (file+head "stat419/${slug}.org"
+                            "#+title: ${title}\n") ;; stat419 notes
+         :immediate-finish t
+         :unnarrowed t)
+        ("data" "data452" plain "%?"
+         :if-new (file+head "data452/${slug}.org"
+                            "#+title: ${title}\n") ;; data452 notes
+         :immediate-finish t
+         :unnarrowed t)
+        ("gwr" "gwr" plain "%?"
+         :if-new (file+head "gwr/${slug}.org"
+                            "#+title: ${title}\n") ;; gwr notes/essays?
+         :immediate-finish t
+         :unnarrowed t)
+        ("cs" "csc477" plain "%?"
+         :if-new (file+head "csc477/${slug}.org"
+                            "#+title: ${title}\n") ;; csc477 notes
+         :immediate-finish t
+         :unnarrowed t)))
+
+(use-package org-roam
+  :ensure t
+  :custom
+  (org-roam-directory (file-truename "~/notes/roam/"))
+  :bind (("C-c n l" . org-roam-buffer-toggle) ;; keybindings
+         ("C-c n f" . org-roam-node-find)
+         ("C-c n g" . org-roam-graph)
+         ("C-c n i" . org-roam-node-insert)
+         ("C-c n c" . org-roam-capture)
+         ;; Dailies
+         ("C-c n j" . org-roam-dailies-capture-today))
+  :config
+  ;; If you're using a vertical completion framework, you might want a more informative completion interface
+  (setq org-roam-node-display-template (concat "${title:*} " (propertize "${tags:10}" 'face 'org-tag)))
+  (org-roam-db-autosync-mode)
+  ;; If using org-roam-protocol
+  (require 'org-roam-protocol))
+
+(use-package! org-roam-ui
+  :after org-roam
+  :hook (org-roam-mode . org-roam-ui-mode)
+  :config
+  (setq org-roam-ui-sync-theme t
+        org-roam-ui-follow t
+        org-roam-ui-update-on-save t
+        org-roam-ui-open-on-start t))
+
+
+(use-package vterm ;; vterm is a terminal emulator
+  :ensure t
+  :commands vterm)
+
+(general-define-key
+ :states 'insert
+ "q" (general-key-dispatch 'self-insert-command
+       :timeout 0.25
+       "q" 'toggle-input-method))
 
 
 ;; Whenever you reconfigure a package, make sure to wrap your config in an
